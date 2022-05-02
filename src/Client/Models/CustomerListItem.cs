3a90d43ee01b149bae04f1954d9e063651c49bc6
Fanie Reynders <fanie@reynders.co>
2022-05-02 20:25:43 +0200

    added more screens

---
+++ src/Client/Models/CustomerListItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+using System.Collections.Generic;
+namespace JobCloud.Client.Models
+{
+    public class CustomerListItem
+    {
+        public string AccountNumber { get; set; }
+        public string CustomerName { get; set; }
+        public string Telephone { get; set; }
+        public string Mobile { get; set; }
+        public string EMail { get; set; }
+    }
+}
+
+
+
