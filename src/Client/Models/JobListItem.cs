2ac0c2a803c56827d4b9ad3281f06e4a2af8143f
Fanie Reynders <fanie@reynders.co>
2022-05-02 17:39:29 +0200

    added jobs grid

---
+++ src/Client/Models/JobListItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+using System.Collections.Generic;
+namespace JobCloud.Client.Models
+{
+    public class JobListItem
+    {
+        public string Id { get; set; }
+        public string JobNumber { get; set; }
+        public DateTime Date { get; set; }
+        public string Customer { get; set; }
+        public string Team { get; set; }
+        public string LoggedBy { get; set; }
+    }
+}
+
+
+
