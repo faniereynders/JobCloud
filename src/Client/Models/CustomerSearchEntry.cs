323118f6438acfc23c36c34a331c0cf1f2ef63d6
Fanie Reynders <fanie@reynders.co>
2022-05-10 20:30:22 +0200

    small tweaks

---
+++ src/Client/Models/CustomerSearchEntry.cs
@@ -0,0 +1,11 @@
+﻿using JobCloud.Shared.Models;
+
+namespace JobCloud.Client.Models
+{
+    public class CustomerSearchEntry
+    {
+        public CustomerType Type { get; set; }
+        public string AccountNumber { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
