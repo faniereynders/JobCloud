e07ac685d18d3f536d1087eba9f431e8a1ddf16b
Fanie Reynders <fanie@reynders.co>
2022-05-10 22:55:29 +0200

    started with customer entry

---
+++ src/Client/Models/CustomerForm.cs
@@ -0,0 +1,28 @@
+﻿using JobCloud.Shared.Models;
+
+namespace JobCloud.Client.Models
+{
+    public class CustomerForm
+    {
+        public string Id { get; set; }
+        public CustomerType Type { get; set; }
+        public string Name { get; set; }
+        public string AccountNumber { get; set; }
+        public string PostalAddress { get; set; }
+        public string ResidentualAddress { get; set; }
+        public List<KeyValuePair<string, string>> ContactDetails => new();
+        public string Email { get; set; }
+        public string IndividualIDNumber { get; set; }
+        public string CompanyRegistrationNumber { get; set; }
+        public string CompanyVATNumber { get; set; }
+        public string CompanyManagerName { get; set; }
+        public List<KeyValuePair<string, string>> CompanyDirectors => new();
+
+        public CustomerForm()
+        {
+            
+                    
+        }
+
+    }
+}
